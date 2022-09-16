55f18e5296e3e823983c9880aab627a9a3aa89a3
github20032003 <209X1A05H6@GPREC.AC.IN>
2022-09-16 20:43:27 +0530

    don2

--- datacollection.py
+++ datacollection.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import os
 df=pd.read_csv('https://data.gov.in/sites/default/files/all_india_PO_list_without_APS_offices_ver2_lat_long.csv')
-df.to_csv('all_india_pincodes.csv')
+
+save_path = 'files/datasets/'
+df.to_csv(os.path.join(save_path,"all_india_city_pincodes.csv"))
+
 print("success")
