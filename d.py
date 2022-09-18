5193f6f588c7e71cc447ff9843ae249fb1dd13f1
github20032003 <209X1A05H6@GPREC.AC.IN>
2022-09-18 12:42:22 +0530

    done
    done

--- d.py
+++ d.py
@@ -37,6 +37,7 @@ def get_data(pin,lat,lon):
       pf.loc[pf["postal_code"] == pin, "N02"] = data["NO2"]['concentration'] 
       pf.loc[pf["postal_code"] == pin, "AQI"] = data['overall_aqi']
       pf.loc[pf["postal_code"] == pin, "PM10"] = data["PM10"]['concentration']
+      print(data)
     except  Exception as e:
       print(data)
       print("Error-",e)
