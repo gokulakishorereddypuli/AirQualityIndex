9db0b014cf28f416fe245cbc75c559823b6a76aa
github20032003 <209X1A05H6@GPREC.AC.IN>
2022-09-18 21:35:59 +0530

    done

--- c.py
+++ c.py
@@ -59,20 +59,19 @@ pf=pf.iloc[29001:30001]
 pf=pf[['postal_code','latitude','longitude']].apply(lambda x : get_data(*x),axis=1)
 
 """
-df1=pd.read_csv('files/datasets/13000_latest_aqi_reports_2022-09-18.csv')
-df2=pd.read_csv('files/datasets/17000_latest_aqi_reports_2022-09-18.csv')
-df3=pd.read_csv('files/datasets/18000_latest_aqi_reports_2022-09-18.csv')
-df4=pd.read_csv('files/datasets/19000_latest_aqi_reports_2022-09-18.csv')
-df5=pd.read_csv('files/datasets/20000_latest_aqi_reports_2022-09-18.csv')
-df6=pd.read_csv('files/datasets/21500_latest_aqi_reports_2022-09-18.csv')
-df7=pd.read_csv('files/datasets/23000_latest_aqi_reports_2022-09-18.csv')
-df8=pd.read_csv('files/datasets/26000_latest_aqi_reports_2022-09-18.csv')
-df9=pd.read_csv('files/datasets/27500_latest_aqi_reports_2022-09-18.csv')
-df10=pd.read_csv('files/datasets/29000_latest_aqi_reports_2022-09-18.csv')
-df11=pd.read_csv('files/datasets/30000_latest_aqi_reports_2022-09-18.csv')
-
-
+df1=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/13000_latest_aqi_reports_2022-09-18.csv')
+df2=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/17000_latest_aqi_reports_2022-09-18.csv')
+df3=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/18000_latest_aqi_reports_2022-09-18.csv')
+df4=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/19000_latest_aqi_reports_2022-09-18.csv')
+df5=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/20000_latest_aqi_reports_2022-09-18.csv')
+df6=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/21500_latest_aqi_reports_2022-09-18.csv')
+df7=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/23000_latest_aqi_reports_2022-09-18.csv')
+df8=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/26000_latest_aqi_reports_2022-09-18.csv')
+df9=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/27500_latest_aqi_reports_2022-09-18.csv')
+df10=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/29000_latest_aqi_reports_2022-09-18.csv')
+df11=pd.read_csv('https://raw.githubusercontent.com/PULI-GOKULA-KISHORE-REDDY/IBM-HACK-CHALLENGE/main/files/datasets/30000_latest_aqi_reports_2022-09-18.csv')
 
 df=pd.concat([df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11])
-save_path = 'files/datasets/'
-df.to_csv(os.path.join(save_path,str("30000")+"_latest_aqi_reports_"+str( date.today())+".csv"))
+print(df.shape)
+x=df.to_csv(str("30000")+"_latest_aqi_reports_"+str( date.today())+".csv")
+print(x)
