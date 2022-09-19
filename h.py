dc6015b23f859faea347292b80c27b3fff4c612f
github20032003 <209X1A05H6@GPREC.AC.IN>
2022-09-19 22:12:50 +0530

    done

--- h.py
+++ h.py
@@ -5,10 +5,8 @@ import json
 from datetime import date
 
 
-df=pd.concat([pd.read_csv('9000_latest_aqi_reports_2022-09-17.csv'),
-pd.read_csv('30000_latest_aqi_reports_2022-09-18.csv'),
-pd.read_csv('31500-69001_latest_aqi_reports_2022-09-19.csv')
-
+df=pd.concat([pd.read_csv('a.csv'),
+pd.read_csv('b.csv')
 ])
-df.to_csv(os.path.join(str(df.shape)+"_latest_aqi_reports_"+str( date.today())+".csv"))
+df.to_csv(str(df.shape)+"_latest_aqi_reports_"+str( date.today())+".csv")
 
