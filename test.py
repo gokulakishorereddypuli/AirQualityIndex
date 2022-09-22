2daeedf06a8503fcc675168856f6c76023020049
github20032003 <209X1A05H6@GPREC.AC.IN>
2022-09-22 23:49:39 +0530

    DONE

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 import os
-print(os.getcwd())
+import pandas as pd
+
+df=pd.read_csv('files/datasets/station_day.csv')
+print(df.isnull().sum())
+df['PM2.5']=df['PM2.5'].fillna(df['PM2.5'].mean())
+df['PM10']=df['PM10'].fillna(df['PM10'].mean())
+df['NO']=df['NO'].fillna(df['NO'].mean())
+df['NO2']=df['NO2'].fillna(df['NO2'].mean())
+df['NOx']=df['NOx'].fillna(df['NOx'].mean())
+df['NH3']=df['NH3'].fillna(df['NH3'].mean())
+df['CO']=df['CO'].fillna(df['CO'].mean())
+df['SO2']=df['SO2'].fillna(df['SO2'].mean())
+df['O3']=df['O3'].fillna(df['O3'].mean())
+df['Benzene']=df['Benzene'].fillna(df['Benzene'].mean())
+df['Toluene']=df['Toluene'].fillna(df['Toluene'].mean())
+df['Xylene']=df['Xylene'].fillna(df['Xylene'].mean())
+print(df.isnull().sum())
